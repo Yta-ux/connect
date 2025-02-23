ad00e117ff7f52c326ed74bdd738c3cbbbb6d4f2
Italo Gustavo <itamelo555@gmail.com>
2025-02-23 12:41:13 -0300

    implement redis and postegres bd

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,22 +1,31 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
   app.post(
     '/subscriptions',
     {
       schema: {
+        summary: 'Subscribe to event',
+        tags: ['subscriptions'],
         body: z.object({
           name: z.string(),
           email: z.string().email(),
         }),
+        response: {
+          201: z.object({ subscriberId: z.string() }),
+        },
       },
     },
     async (request, reply) => {
       const { name, email } = request.body
-      return reply.status(201).send({
+      const { subscriberId } = await subscribeToEvent({
         name,
         email,
+      })
+      return reply.status(201).send({
+        subscriberId,
       })
     }
   )
